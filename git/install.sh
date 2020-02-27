d6907eabc717a9140db8ccfdf7315bc3d4854ee5
numToStr <vikasraj1911@gmail.com>
2020-02-27 22:43:11 +0530

    new: added git

---
+++ git/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# If hub is found, use https: instead of git: or ssh::
+if command -v hub >/dev/null 2>&1; then
+    git config --global hub.protocol https
+fi
+
+# use vscode as mergetool
+if command -v code >/dev/null 2>&1; then
+	git config --global merge.tool vscode
+	git config --global mergetool.vscode.cmd "code --wait $MERGED"
+fi
