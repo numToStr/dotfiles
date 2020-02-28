8ca3ddc0bd1319c54a1cf7afcf33d1df3674575e
numToStr <vikasraj1911@gmail.com>
2020-02-28 18:30:55 +0530

    Revert "ohmyzsh: moved plugins into config.zsh"
    This reverts commit b3985be007555a4288b6ccaeafdd67bd2532c84b.

---
+++ oh-my-zsh/plugins.zsh
@@ -0,0 +1,19 @@
+# Which plugins would you like to load?
+# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(
+    # zsh-syntax-highlighting
+    # zsh-completions
+    # history-substring-search
+    # zsh-autosuggestions
+    git
+    docker
+    docker-compose
+    archlinux
+    tmux
+    # vi-mode
+)
+
+
