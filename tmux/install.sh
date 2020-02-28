0484a2009ecbcfd85161990f4a5dcd31fba94e35
numToStr <vikasraj1911@gmail.com>
2020-02-28 18:43:10 +0530

    fix: tmux.conf symlink

--- tmux/install.sh
+++ tmux/install.sh
@@ -5,5 +5,5 @@ echo "You should install 'tmux' through distro's package manager"
 if command -v tmux >/dev/null 2>&1; then
     echo "::[ Linking ] 'tmux.conf'"
     # touch ~/.tmux.conf
-    ln -sf "$DOTFILES/tmux/tmux.conf" ~/.tmux.conf
+    ln -sf "$DOTFILES/tmux/tmux.conf.symlink" ~/.tmux.conf
 fi
