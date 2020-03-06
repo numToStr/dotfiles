114da14b56f1734d15ce9f7ae22f1287da1de4a0
numToStr <vikasraj1911@gmail.com>
2020-03-06 12:33:06 +0530

    yarn: fixed multiple installation

--- yarn/install.sh
+++ yarn/install.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# if [ command -v yarn >/dev/null 2>&1 ]
-# then
-#     echo "::[ Already Installed ] 'yarn'"
-# else
+if command -v yarn >/dev/n; then
+    echo "::[ Already Installed ] 'yarn'"
+else
     echo "::[ Installing ] 'yarn'"
     curl -o- -L https://yarnpkg.com/install.sh | bash 
-# fi
+fi
 
