b7ab3861cc2636bff45f553783ae7a73b8cd5054
numToStr <vikasraj1911@gmail.com>
2020-07-19 15:01:57 +0530

    change(tmux): added my personal theme and some plugins

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,6 +4,6 @@ echo "You should install 'tmux' through distro's package manager"
 
 if command -v tmux >/dev/null 2>&1; then
     echo "::[ Linking ] 'tmux.conf'"
-    # touch ~/.tmux.conf
+    ln -sf "$DOTFILES/tmux/tmux.dawn.conf.symlink" ~/.tmux.dawn.conf
     ln -sf "$DOTFILES/tmux/tmux.conf.symlink" ~/.tmux.conf
 fi
