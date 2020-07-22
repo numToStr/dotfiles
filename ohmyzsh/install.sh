72ce0831e2370452e83e141f42578fa868a07020
numToStr <vikasraj1911@gmail.com>
2020-07-22 19:17:59 +0530

    feat(ohmyzsh): if already installed will update

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -3,40 +3,41 @@
 OMZ_DIR=$HOME/.oh-my-zsh
 OMZ_FILE=$OMZ_DIR/oh-my-zsh.sh
 
-if [[ -d "$OMZ_DIR" && -f "$OMZ_FILE" ]];
-then
-    echo "::[ Already Installed ] 'Oh my zsh'"
-    # echo "::[ Updating ] 'Oh my zsh'"
-    # $OMZ_FILE && upgrade_oh_my_zsh
-else
-    echo "::[ Installing ] 'Oh my zsh'"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+MODULE="Oh-My-Zsh"
 
-    if command -v zsh >/dev/null 2>&1; then
-        echo "::[ Linking ] '.zshrc'"
+if command -v zsh >/dev/null 2>&1; then
+    if [[ -d "$OMZ_DIR" && -f "$OMZ_FILE" ]]; then
+        echo "[ UPDATING ] :: $MODULE"
+        sh $OMZ_DIR/tools/upgrade.sh
+        command rm -rf $OMZ_DIR/log/update.lock
+    else
+        echo "[ INSTALLING ] :: $MODULE"
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+        echo "[ LINKING ] :: $MODULE >> .zshrc"
         mv ~/.zshrc ~/.zshrc.backup
         ln -sf "$DOTFILES/ohmyzsh/zshrc.symlink" ~/.zshrc
+
+        # echo "::[ Installing ] Plugin >> 'zsh-syntax-highlighting'"
+        # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+        echo "[ INSTALLING ] :: $MODULE >> Plugin >> fast-syntax-highlighting"
+        git clone https://github.com/zdharma/fast-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+        echo "[ INSTALLING ] :: $MODULE >> Plugin >> zsh-autosuggestions"
+        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+        echo "[ INSTALLING ] :: $MODULE >> Plugin >> zsh-completions"
+        git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+        # autoload -U compinit && compinit
+
+        echo "[ INSTALLING ] :: $MODULE >> Plugin >> zsh-history-substring-search"
+        git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+        echo "[ INSTALLING ] :: $MODULE >> Theme >> spaceship-prompt"
+        git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+        ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+        # Set ZSH_THEME="spaceship" in your .zshrc.
     fi
-
-    # echo "::[ Installing ] Plugin >> 'zsh-syntax-highlighting'"
-    # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-    echo "::[ Installing ] Plugin >> 'fast-syntax-highlighting'"
-    git clone https://github.com/zdharma/fast-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-
-    echo "::[ Installing ] Plugin >> 'zsh-autosuggestions'"
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-    echo "::[ Installing ] Plugin >> 'zsh-completions'"
-    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-    # autoload -U compinit && compinit
-
-    echo "::[ Installing ] Plugin >> 'zsh-history-substring-search'"
-    git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-
-    echo "::[ Installing ] Theme >> 'spaceship prompt'"
-    git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-    # Set ZSH_THEME="spaceship" in your .zshrc.
 fi
