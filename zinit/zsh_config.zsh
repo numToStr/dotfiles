ef6dd77c62ddcbadd44d5b18188937ac25096b07
numToStr <vikasraj1911@gmail.com>
2020-09-18 21:00:52 +0530

    new(zsh): introduce zinit

--- zinit/zsh_config.zsh
+++ zinit/zsh_config.zsh
@@ -34,6 +34,7 @@ SAVEHIST=$HISTSIZE
 # ENV               #
 #####################
 ZSH_AUTOSUGGEST_MANUAL_REBIND=1  # make prompt faster
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 DISABLE_MAGIC_FUNCTIONS=true     # make pasting into terminal faster
 export EDITOR=nvim
 export PAGER=bat
