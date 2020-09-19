5b91f097d364047010a04e04a0dae028e051a55b
numToStr <vikasraj1911@gmail.com>
2020-09-19 15:13:59 +0530

    fix: symlinks path

--- git/install.sh
+++ git/install.sh
@@ -2,6 +2,10 @@
 
 echo "[ LINKING ] :: Git >> config"
 
+ln -sf "$DOTFILES/git/.gitconfig.local.symlink" $HOME/.gitconfig.local
+
+git config --global include.path $HOME/.gitconfig.local
+
 # If hub is found, use https: instead of git: or ssh::
 if command -v hub >/dev/null 2>&1; then
     git config --global hub.protocol https
