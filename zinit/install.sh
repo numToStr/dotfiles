d9938a7a1ef8b1d0ecbfbc40654a349a455959fa
numToStr <vikasraj1911@gmail.com>
2020-09-19 16:43:54 +0530

    fix(zsh): zinit command not found while updating

--- zinit/install.sh
+++ zinit/install.sh
@@ -2,27 +2,23 @@
 
 if command -v zsh >/dev/null 2>&1; then
     MODULE='Zsh'
+    ZINIT=$HOME/.zinit/bin/zinit.zsh
 
-    if [[ -f $HOME/.zinit/bin/zinit.zsh ]]; then
-        echo "[ UPDATING ] :: $MODULE >> zinit"
-        zinit self-update
+    echo "[ LINKING ] :: $MODULE >> .zshrc"
 
-        echo "[ UPDATING ] :: $MODULE >> Plugins"
-        zinit update --all
-    else
+    [[ -f $HOME/.zshrc ]] && mv $HOME/.zshrc $HOME/.zshrc.backup
+    ln -sf "$DOTFILES/zinit/.zshrc.symlink" $HOME/.zshrc
+
+    if [[ ! -f $ZINIT ]]; then
         echo "[ INSTALLING ] :: $MODULE >> zinit"
 
-        print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
         command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
         command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
-            print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
-            print -P "%F{160}▓▒░ The clone has failed.%f%b"
-
-        echo "[ LINKING ] :: $MODULE >> .zshrc"
-
-        [[ -f $HOME/.zshrc ]] && mv $HOME/.zshrc $HOME/.zshrc.backup
-        ln -sf "$DOTFILES/zinit/.zshrc.symlink" $HOME/.zshrc
+            echo "[ SUCCESS ] :: Installation successful" || \
+            echo "[ FAIL ] :: Installation failed"
 
         echo "[ NOTE ] :: All $MODULE plugins will be installed on first prompt."
+    else
+        echo "[ Info ] :: Run 'zinit self-update && zinit update --all' to update plugins"
     fi
 fi
