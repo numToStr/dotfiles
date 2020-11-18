08fa4241e98985be02b0e516e7d0110a9bccfb7a
numToStr <vikasraj1911@gmail.com>
2020-11-18 16:26:10 +0530

    change(nvim): replace `table.merge` with `vim.tbl_extend()`

--- neovim/lua/utils.lua
+++ neovim/lua/utils.lua
@@ -3,20 +3,20 @@ local api = vim.api
 local cmd = vim.cmd
 local U = {}
 
-function table.merge(dest, src)
-   for k,v in pairs(src) do
-       dest[k] = v
-   end
-   return dest
-end
+-- function table.merge(dest, src)
+--    for k,v in pairs(src) do
+--        dest[k] = v
+--    end
+--    return dest
+-- end
 
 -- Key mapping
 function U.map(mode, key, result, opts)
-    opts = table.merge({
+    opts = vim.tbl_extend('keep', opts or {}, {
         noremap = true,
         silent = true,
         expr = false
-    }, opts or {})
+    })
 
     fn.nvim_set_keymap(mode, key, result, opts)
 end
