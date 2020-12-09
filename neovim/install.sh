83359e5137322a1bb656c747539ee5f5460bc2ef
numToStr <vikasraj1911@gmail.com>
2020-12-09 12:14:45 +0530

    change(nvim): replace vim-plug => packer.nvim

--- neovim/install.sh
+++ neovim/install.sh
@@ -2,20 +2,24 @@
 
 if command -v nvim >/dev/null 2>&1; then
     MODULE="NeoVim"
-    PLUG_FILE=~/.local/share/nvim/site/autoload/plug.vim
+    PACKER=$HOME/.local/share/nvim/site/pack/packer/opt/packer.nvim
 
     if [ ! -f $PLUG_FILE ]; then
-        echo "[ INSTALLING ] :: $MODULE >> vim-plug"
-        curl -fLo $PLUG_FILE --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        echo "[ INSTALLING ] :: $MODULE >> packer.nvim"
+
+        git clone https://github.com/wbthomason/packer.nvim $PACKER
 
         mkdir -p $HOME/.config/nvim/
 
-        echo "[ LINKING ] :: $MODULE >> init.vim"
-        ln -sf "$DOTFILES/neovim/init.vim" $HOME/.config/nvim/init.vim
-    fi
+        echo "[ LINKING ] :: $MODULE >> init.lua"
+        ln -sf "$DOTFILES/neovim/init.lua" $HOME/.config/nvim/init.lua
 
-    echo "[ UPDATING ] :: $MODULE >> Plugins"
-	nvim +'PlugUpdate --sync' +qa
+        echo "[ INSTALLING ] :: $MODULE >> Plugins"
+        nvim --headless +PackerCompile +PackerInstall +qa
+    else
+        echo "[ UPDATING ] :: $MODULE >> Plugins"
+        nvim --headless +PackerUpdate +qa
+    fi
 else
     echo "[ ERROR ] :: Please install `neovim`"
 fi
