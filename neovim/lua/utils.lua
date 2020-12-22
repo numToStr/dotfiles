dfbe2765d09a59106f20e434be7e377355465ea0
numToStr <vikasraj1911@gmail.com>
2020-12-22 16:26:12 +0530

    nvim: autocmd vol.2

--- neovim/lua/utils.lua
+++ neovim/lua/utils.lua
@@ -90,9 +90,9 @@ function U.define_autocmd(spec)
     local once = spec.once and "++once" or ""
     local nested = spec.nested and "++nested" or ""
 
-    local action = spec.command or ''
+    local command = spec.command or ''
 
-    cmd(join("autocmd", group, event, pattern, once, nested, action))
+    cmd(join("autocmd", group, event, pattern, once, nested, command))
 end
 
 return U
