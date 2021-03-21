46d9bf667e6a09b32677e4268ad430d60040f00d
numToStr <vikasraj1911@gmail.com>
2021-03-21 12:39:40 +0530

    fmt

--- neovim/lua/plugins/_treesitter.lua
+++ neovim/lua/plugins/_treesitter.lua
@@ -1,48 +1,50 @@
-require('nvim-treesitter.configs').setup({
-    ensure_installed = 'maintained',
-    playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = false -- Whether the query persists across vim sessions
-    },
-    highlight = {
-        enable = true
-    },
-    indent = {
-        enable = true
-    },
-    textobjects = {
-        select = {
+require("nvim-treesitter.configs").setup(
+    {
+        ensure_installed = "maintained",
+        playground = {
             enable = true,
-            keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
-                ["al"] = "@loop.outer",
-                ["il"] = "@loop.inner",
+            disable = {},
+            updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+            persist_queries = false -- Whether the query persists across vim sessions
+        },
+        highlight = {
+            enable = true
+        },
+        indent = {
+            enable = true
+        },
+        textobjects = {
+            select = {
+                enable = true,
+                keymaps = {
+                    -- You can use the capture groups defined in textobjects.scm
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
+                    ["al"] = "@loop.outer",
+                    ["il"] = "@loop.inner"
 
-                -- Or you can define your own textobjects like this
-                -- ["iF"] = {
-                --     python = "(function_definition) @function",
-                --     cpp = "(function_definition) @function",
-                --     c = "(function_definition) @function",
-                --     java = "(method_declaration) @function",
-                -- },
+                    -- Or you can define your own textobjects like this
+                    -- ["iF"] = {
+                    --     python = "(function_definition) @function",
+                    --     cpp = "(function_definition) @function",
+                    --     c = "(function_definition) @function",
+                    --     java = "(method_declaration) @function",
+                    -- },
+                }
             },
-        },
-        swap = {
-            enable = true,
-            swap_next = {
-                ["<leader>a"] = "@parameter.inner",
-            },
-            swap_previous = {
-                ["<leader>A"] = "@parameter.inner",
-            },
-        },
-    },
-})
+            swap = {
+                enable = true,
+                swap_next = {
+                    ["<leader>a"] = "@parameter.inner"
+                },
+                swap_previous = {
+                    ["<leader>A"] = "@parameter.inner"
+                }
+            }
+        }
+    }
+)
 -- vim.cmd('set foldmethod=expr')
 -- vim.cmd('set foldexpr=nvim_treesitter#foldexpr()')
