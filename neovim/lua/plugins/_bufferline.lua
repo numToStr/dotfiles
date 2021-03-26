f597902658894e22ecfae0cf037c5b259c881516
numToStr <vikasraj1911@gmail.com>
2021-03-26 23:31:41 +0530

    fix(nvim): bufferline config not working

--- neovim/lua/plugins/_bufferline.lua
+++ neovim/lua/plugins/_bufferline.lua
@@ -3,10 +3,13 @@ local M = {}
 function M.config()
     require "bufferline".setup(
         {
-            show_buffer_close_icons = false,
-            show_close_icon = false,
-            enforce_regular_tabs = true,
-            max_prefix_length = 10
+            options = {
+                show_buffer_close_icons = false,
+                show_close_icon = false,
+                enforce_regular_tabs = true,
+                -- max_prefix_length = 30, -- prefix used when a buffer is deduplicated
+                tab_size = 25
+            }
         }
     )
 end
