1b40c42f98db533359ca330cfb543d18b33722a6
numToStr <vikasraj1911@gmail.com>
2021-04-12 10:31:49 +0530

    change(nvim): removed git-gutter highlights from gitsigns

--- neovim/lua/plugins/_gitsigns.lua
+++ neovim/lua/plugins/_gitsigns.lua
@@ -4,24 +4,9 @@ function M.config()
     require("gitsigns").setup(
         {
             signs = {
-                add = {
-                    hl = "GitGutterAdd",
-                    text = "+"
-                },
-                change = {
-                    hl = "GitGutterChange",
-                    text = "~"
-                },
-                delete = {
-                    hl = "GitGutterDelete"
-                },
-                topdelete = {
-                    hl = "GitGutterDelete"
-                },
-                changedelete = {
-                    hl = "GitGutterChange",
-                    text = "="
-                }
+                add = {text = "+"},
+                change = {text = "~"},
+                changedelete = {text = "="}
             }
         }
     )
