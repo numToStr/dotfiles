b6c00fe2604194592c68863cd50bda916865528e
numToStr <vikasraj1911@gmail.com>
2021-04-22 16:06:04 +0530

    nvim: exclude more {buf,file}type in indentline

--- neovim/lua/plugins/_indentline.lua
+++ neovim/lua/plugins/_indentline.lua
@@ -4,8 +4,8 @@ function M.config()
     local g = vim.g
 
     g.indent_blankline_show_first_indent_level = false
-    g.indent_blankline_filetype_exclude = {"help"}
-    g.indent_blankline_buftype_exclude = {"terminal"}
+    g.indent_blankline_filetype_exclude = {"help", "packer", "FTerm"}
+    g.indent_blankline_buftype_exclude = {"terminal", "nofile"}
 end
 
 return M
