7fac42085ab73d86a51dd9b720b842a737279057
numToStr <vikasraj1911@gmail.com>
2021-06-19 19:47:27 +0530

    nvim: remap hop keybinds to HopWord{AC,BC}

--- neovim/lua/plugins/_hop.lua
+++ neovim/lua/plugins/_hop.lua
@@ -3,12 +3,8 @@ local M = {}
 function M.config()
     local U = require "utils"
 
-    -- U.map("n", "<S-m>", "<CMD>lua require'hop'.hint_words()<CR>")
-
-    -- These are mapping are legacy reasons, because I used them with easymotion
-    -- Maybe I remove them in future, once I used to the above mapping
-    U.map("n", "<S-J>", "<CMD>lua require'hop'.hint_words()<CR>")
-    U.map("n", "<S-K>", "<CMD>lua require'hop'.hint_words()<CR>")
+    U.map("n", "<S-J>", "<CMD>HopWordAC<CR>")
+    U.map("n", "<S-K>", "<CMD>HopWordBC<CR>")
 end
 
 return M
