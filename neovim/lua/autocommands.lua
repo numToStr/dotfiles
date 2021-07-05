0f103ac9dfc7ccf6df4366d044b18eb33fd0d6f1
numToStr <vikasraj1911@gmail.com>
2021-07-05 15:21:12 +0530

    nvim: specify mode in keymap for help page

--- neovim/lua/autocommands.lua
+++ neovim/lua/autocommands.lua
@@ -43,17 +43,12 @@ end
 
 au.augroup("MyFileTypes", au_filetypes)
 
--- Appending the extra file types for vim-snippets to show react snippets
--- Messing with prettier
--- cmd("au BufRead,BufNewFile *.jsx setlocal filetype=javascript.javascriptreact.javascript_react")
--- cmd("au BufRead,BufNewFile *.tsx setlocal filetype=typescript.typescriptreact.javascript_react")
-
 -- Open help vertically and press q to exit
 local function help_tab()
     if vim.bo.buftype == "help" then
         cmd("wincmd L")
         local nr = api.nvim_get_current_buf()
-        api.nvim_buf_set_keymap(nr, "", "q", ":q<CR>", {noremap = true, silent = true})
+        api.nvim_buf_set_keymap(nr, "n", "q", ":q<CR>", {noremap = true, silent = true})
     end
 end
 
