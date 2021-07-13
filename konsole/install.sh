6cfeef29641642dff806a8fef9914e44b688ffd3
numToStr <vikasraj1911@gmail.com>
2021-07-13 13:50:54 +0530

    konsole: prepare for stow

--- konsole/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if command -v konsole >/dev/null 2>&1; then
-    MODULE='Konsole'
-
-    echo "[ LINKING ] :: $MODULE >> nord.colorscheme"
-    ln -sf "$DOTFILES/konsole/node.colorscheme.symlink" ~/.local/share/konsole/nord.colorscheme
-fi
