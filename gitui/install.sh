188f84e1e8e1e299ca3bd42dff927a0ebe0e49ed
Vikas Raj <vikasraj1911@gmail.com>
2021-07-14 18:04:09 +0530

    feat: use `stow` to manage dotfiles (#8)

--- gitui/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-MODULE='gitui'
-
-echo "[ LINKING ] :: $MODULE >> {theme,keybinds}"
-
-ln -sf "$DOTFILES/gitui/theme.ron" $HOME/.config/gitui/theme.ron
-ln -sf "$DOTFILES/gitui/key_config.ron" $HOME/.config/gitui/key_config.ron
