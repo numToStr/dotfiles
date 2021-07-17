57f87c4bcba700ca26d2d16fd9c82e029898c64d
numToStr <vikasraj1911@gmail.com>
2021-07-17 19:51:39 +0530

    nvim: disable vim regex highlighting when treesitter is enable

--- neovim/.config/nvim/lua/plugins/_treesitter.lua
+++ neovim/.config/nvim/lua/plugins/_treesitter.lua
@@ -6,6 +6,11 @@ function M.config()
         ensure_installed = 'all',
         highlight = {
             enable = true,
+            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+            -- Using this option may slow down your editor, and you may see some duplicate highlights.
+            -- Instead of true it can also be a list of languages
+            additional_vim_regex_highlighting = false,
         },
         indent = {
             enable = true,
