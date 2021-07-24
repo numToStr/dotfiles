0b2a03225949c618073dc7cd28e6a73ebeb9c806
numToStr <vikasraj1911@gmail.com>
2021-07-24 15:01:51 +0530

    remove old plugin files

--- neovim/.config/nvim/lua/plugins/_statusline.lua
+++
@@ -1,8 +0,0 @@
-local g = vim.g
-
-g.airline_theme = 'gruvbox_material'
-g.airline_powerline_fonts = 1
-g.airline_highlighting_cache = 1
-g['airline#extensions#tabline#enabled'] = 1
-g['airline#extensions#tabline#formatter'] = 'unique_tail'
-g['airline#extensions#tmuxline#enabled'] = 0
