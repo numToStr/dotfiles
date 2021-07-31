f60f21c47a0a08735c96bd3aa4249a08ad30fc4d
numToStr <vikasraj1911@gmail.com>
2021-07-31 12:39:28 +0530

    nvim: update treesitter inc-selection keybinds

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -15,10 +15,10 @@ require('nvim-treesitter.configs').setup({
     incremental_selection = {
         enable = true,
         keymaps = {
-            init_selection = 'gnn',
-            node_incremental = 'grn',
-            scope_incremental = 'grc',
-            node_decremental = 'grN',
+            init_selection = '<leader>si',
+            node_incremental = '<leader>sn',
+            node_decremental = '<leader>sN',
+            scope_incremental = '<leader>sc',
         },
     },
     -- nvim-treesitter/nvim-treesitter-textobjects
