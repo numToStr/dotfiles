bea1d4148a5f47ccdc1550baecf9df1901b5bcac
numToStr <vikasraj1911@gmail.com>
2021-08-17 11:33:17 +0530

    nvim: change * behaviour

--- neovim/.config/nvim/lua/keybinds.lua
+++ neovim/.config/nvim/lua/keybinds.lua
@@ -1,5 +1,8 @@
 local U = require('utils')
 
+-- Fix * (Keep the cursor position, don't move to next match)
+U.map('n', '*', '*N')
+
 -- Fix n and N. Keeping cursor in center
 U.map('n', 'n', 'nzz')
 U.map('n', 'N', 'Nzz')
