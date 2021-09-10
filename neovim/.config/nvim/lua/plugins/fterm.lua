a33d9a3b212d01f1bbcfc6dfdcb5cea37a62ff64
numToStr <vikasraj1911@gmail.com>
2021-09-10 11:59:10 +0530

    nvim: update FTerm.nvim config

--- neovim/.config/nvim/lua/plugins/fterm.lua
+++ neovim/.config/nvim/lua/plugins/fterm.lua
@@ -1,24 +1,6 @@
 local U = require('utils')
 
-require('FTerm').setup()
-
 U.map('n', '<A-i>', "<CMD>lua require('FTerm').toggle()<CR>")
 U.map('t', '<A-i>', "<C-\\><C-n><CMD>lua require('FTerm').toggle()<CR>")
 
--- gitui via FTerm
-local term = require('FTerm.terminal')
-
-local gitui = term:new():setup({
-    cmd = 'gitui',
-    dimensions = {
-        height = 0.95,
-        width = 0.95,
-    },
-})
-
-function _G.__gitui()
-    gitui:toggle()
-end
-
-U.map('n', '<A-g>', '<CMD>lua __gitui()<CR>')
-U.map('t', '<A-g>', '<C-\\><C-n><CMD>lua __gitui()<CR>')
+U.map('n', '<A-g>', '<CMD>lua require("FTerm"):new({ cmd = "gitui" }):open()<CR>')
