09d68ce6ced4368a3e3a439936722ba88085353b
numToStr <vikasraj1911@gmail.com>
2021-09-27 15:24:48 +0530

    nvim: migrate to setup for nvim-tree

--- neovim/.config/nvim/lua/plugins/nvim-tree.lua
+++ neovim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,52 +1,14 @@
 local U = require('utils')
 local g = vim.g
 
-g.nvim_tree_side = 'left'
-g.nvim_tree_width = 35
 g.nvim_tree_ignore = { '.git', 'node_modules' }
-
--- 0 by default, opens the tree when typing `vim $DIR` or `vim`
--- g.nvim_tree_auto_open = 1
-
--- 0 by default, closes the tree when it's the last window
--- g.nvim_tree_auto_close = 0
-
--- 0 by default, this option allows the cursor to be updated when entering a buffer
-g.nvim_tree_follow = 1
-
--- 0 by default, will enable file highlight for git attributes (can be used without the icons).
--- g.nvim_tree_git_hl = 1
-
--- 0 by default, this option shows indent markers when folders are open
 g.nvim_tree_indent_markers = 1
-
--- 0 by default, will enable folder and file icon highlight for opened files/directories.
--- g.nvim_tree_highlight_opened_files = 1
-
--- 0 by default, this option hides files and folders starting with a dot `.`
--- g.nvim_tree_hide_dotfiles = 1
-
--- This is the default. See :help filename-modifiers for more options
--- g.nvim_tree_root_folder_modifier = ':~'
-
--- 0 by default, will open the tree when entering a new tab and the tree was previously open
--- g.nvim_tree_tab_open = 1
-
--- 0 by default, will disable the window picker.
 g.nvim_tree_disable_window_picker = 1
-
--- "If 0, do not show the icons for one of 'git' 'folder' and 'files'
--- "1 by default, notice that if 'files' is 1, it will only display
--- "if nvim-web-devicons is installed and on your runtimepath
--- If I don't set these then a leading indent marker will show bcz of `folder_arrows`
 g.nvim_tree_show_icons = {
     git = 1,
     folders = 1,
     files = 1,
 }
-
--- " default will show icon by default if no icon is provided
--- " default shows no icon by default
 g.nvim_tree_icons = {
     default = '',
     symlink = '',
@@ -59,6 +21,17 @@ g.nvim_tree_icons = {
         deleted = '-',
     },
 }
+
+require('nvim-tree').setup({
+    lsp_diagnostics = true,
+    update_focused_file = {
+        enable = true,
+    },
+    view = {
+        width = 35,
+        side = 'left',
+    },
+})
 
 U.map('n', '<C-n>', ':NvimTreeToggle<CR>')
 
