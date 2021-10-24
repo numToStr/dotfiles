0c7ab3d2e17bc656f5071975fea2ffaeee32a10c
numToStr <vikasraj1911@gmail.com>
2021-10-24 13:59:29 +0530

    fix and remove stuff

--- neovim/.config/nvim/lua/plugins/navigator.lua
+++ neovim/.config/nvim/lua/plugins/navigator.lua
@@ -4,7 +4,7 @@ require('Navigator').setup({
     auto_save = 'current',
 })
 
-U.map('n', '<A-h>', ':lua require("Navigator").left()<CR>')
-U.map('n', '<A-k>', ':lua require("Navigator").up()<CR>')
-U.map('n', '<A-l>', ':lua require("Navigator").right()<CR>')
-U.map('n', '<A-j>', ':lua require("Navigator").down()<CR>')
+U.map('n', '<A-h>', '<CMD>lua require("Navigator").left()<CR>')
+U.map('n', '<A-k>', '<CMD>lua require("Navigator").up()<CR>')
+U.map('n', '<A-l>', '<CMD>lua require("Navigator").right()<CR>')
+U.map('n', '<A-j>', '<CMD>lua require("Navigator").down()<CR>')
--- neovim/.config/nvim/lua/utils.lua
+++ neovim/.config/nvim/lua/utils.lua
@@ -20,64 +20,64 @@ function U.move_cursor_from_tree()
         cmd('wincmd l')
     end
 end
-
--- Stolen from https://github.com/kyazdani42/nvim-palenight.lua/blob/master/lua/palenight.lua#L10
--- Usage:
--- highlight(Cursor, { fg = bg_dark, bg = yellow })
-function U.highlight(group, styles)
-    local gui = styles.gui or 'NONE'
-    local sp = styles.sp or 'NONE'
-    local fg = styles.fg or 'NONE'
-    local bg = styles.bg or 'NONE'
-
-    cmd('highlight! ' .. group .. ' gui=' .. gui .. ' guisp=' .. sp .. ' guifg=' .. fg .. ' guibg=' .. bg)
-end
-
--- Usage:
--- highlight({
---      CursorLine   = { bg = bg },
---      Cursor       = { fg = bg_dark, bg = yellow }
--- })
-function U.highlights(hi_table)
-    for group, styles in pairs(hi_table) do
-        U.highlight(group, styles)
-    end
-end
-
-function U.hiLink(src, dest)
-    cmd('highlight link ' .. src .. ' ' .. dest)
-end
-
-function U.hiLinks(hi_table)
-    for src, dest in pairs(hi_table) do
-        U.hiLink(src, dest)
-    end
-end
-
-local function get_hex(rgb)
-    return '#' .. require('bit').tohex(rgb, 6)
-end
-
--- For getting hex color from hi group
--- Usage:
--- get_hl_color('Normal', 'fg' | 'bg' | 'both')
-function U.get_hl_color(name, attr)
-    local colors = api.nvim_get_hl_by_name(name, true)
-
-    if attr == 'both' then
-        return {
-            fg = get_hex(colors.foreground),
-            bg = get_hex(colors.background),
-        }
-    end
-
-    if attr == 'fg' then
-        return get_hex(colors.foreground)
-    end
-
-    if attr == 'bg' then
-        return get_hex(colors.background)
-    end
-end
+--
+-- -- Stolen from https://github.com/kyazdani42/nvim-palenight.lua/blob/master/lua/palenight.lua#L10
+-- -- Usage:
+-- -- highlight(Cursor, { fg = bg_dark, bg = yellow })
+-- function U.highlight(group, styles)
+--     local gui = styles.gui or 'NONE'
+--     local sp = styles.sp or 'NONE'
+--     local fg = styles.fg or 'NONE'
+--     local bg = styles.bg or 'NONE'
+--
+--     cmd('highlight! ' .. group .. ' gui=' .. gui .. ' guisp=' .. sp .. ' guifg=' .. fg .. ' guibg=' .. bg)
+-- end
+--
+-- -- Usage:
+-- -- highlight({
+-- --      CursorLine   = { bg = bg },
+-- --      Cursor       = { fg = bg_dark, bg = yellow }
+-- -- })
+-- function U.highlights(hi_table)
+--     for group, styles in pairs(hi_table) do
+--         U.highlight(group, styles)
+--     end
+-- end
+--
+-- function U.hiLink(src, dest)
+--     cmd('highlight link ' .. src .. ' ' .. dest)
+-- end
+--
+-- function U.hiLinks(hi_table)
+--     for src, dest in pairs(hi_table) do
+--         U.hiLink(src, dest)
+--     end
+-- end
+--
+-- local function get_hex(rgb)
+--     return '#' .. require('bit').tohex(rgb, 6)
+-- end
+--
+-- -- For getting hex color from hi group
+-- -- Usage:
+-- -- get_hl_color('Normal', 'fg' | 'bg' | 'both')
+-- function U.get_hl_color(name, attr)
+--     local colors = api.nvim_get_hl_by_name(name, true)
+--
+--     if attr == 'both' then
+--         return {
+--             fg = get_hex(colors.foreground),
+--             bg = get_hex(colors.background),
+--         }
+--     end
+--
+--     if attr == 'fg' then
+--         return get_hex(colors.foreground)
+--     end
+--
+--     if attr == 'bg' then
+--         return get_hex(colors.background)
+--     end
+-- end
 
 return U
