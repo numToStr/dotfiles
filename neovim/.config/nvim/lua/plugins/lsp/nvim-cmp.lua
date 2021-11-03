04f18f2ceea9a4828b9172c6e290ab41c2d752bc
numToStr <vikasraj1911@gmail.com>
2021-11-03 23:19:18 +0530

    cmp source priority

--- neovim/.config/nvim/lua/plugins/lsp/nvim-cmp.lua
+++ neovim/.config/nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -9,10 +9,10 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
     },
     sources = cmp.config.sources({
-        { name = 'nvim_lsp', max_item_count = 10 },
-        { name = 'path', max_item_count = 10 },
         { name = 'luasnip', max_item_count = 5 },
-        { name = 'buffer', max_item_count = 8 },
+        { name = 'nvim_lsp', max_item_count = 5 },
+        { name = 'path', max_item_count = 10 },
+        { name = 'buffer', max_item_count = 10 },
     }),
     snippet = {
         expand = function(args)
