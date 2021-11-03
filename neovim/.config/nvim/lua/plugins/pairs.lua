66ffeff78ba6ab0308d091354d8654db2a5a2df9
numToStr <vikasraj1911@gmail.com>
2021-11-03 21:59:56 +0530

    update cmp + autopairs integration

--- neovim/.config/nvim/lua/plugins/pairs.lua
+++ neovim/.config/nvim/lua/plugins/pairs.lua
@@ -1,13 +1,4 @@
 require('nvim-autopairs').setup()
 
--- you need setup cmp first put this after cmp.setup()
-require('nvim-autopairs.completion.cmp').setup({
-    map_cr = true, --  map <CR> on insert mode
-    map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-    auto_select = true, -- automatically select the first item
-    insert = false, -- use insert confirm behavior instead of replace
-    map_char = { -- modifies the function or method delimiter by filetypes
-        all = '(',
-        tex = '{',
-    },
-})
+-- Integration w/ nvim-cmp
+require('cmp').event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
