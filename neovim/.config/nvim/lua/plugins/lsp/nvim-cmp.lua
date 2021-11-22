60851454d03410ac907ee9f3c0cddc50ab465887
numToStr <vikasraj1911@gmail.com>
2021-11-22 14:07:26 +0530

    remove nvim-cmp <C-e> mapping

--- neovim/.config/nvim/lua/plugins/lsp/nvim-cmp.lua
+++ neovim/.config/nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -2,15 +2,15 @@ local cmp = require('cmp')
 
 cmp.setup({
     mapping = {
+        ['<C-e>'] = cmp.config.disable,
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-q>'] = cmp.mapping.close(),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
         ['<C-Space>'] = cmp.mapping.complete(),
     },
     sources = cmp.config.sources({
-        { name = 'luasnip', max_item_count = 5 },
         { name = 'nvim_lsp', max_item_count = 5 },
+        { name = 'luasnip', max_item_count = 5 },
         { name = 'path', max_item_count = 10 },
         { name = 'buffer', max_item_count = 10 },
     }),
