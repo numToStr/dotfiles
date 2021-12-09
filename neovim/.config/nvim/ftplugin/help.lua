f2eb9aaf998ea23bf6dc0f9c6636e3d75ae83063
numToStr <vikasraj1911@gmail.com>
2021-12-09 21:02:23 +0530

    rewrite autocmds

--- neovim/.config/nvim/ftplugin/help.lua
+++
@@ -1,5 +0,0 @@
--- Open help vertically and press q to exit
-if vim.bo.buftype == 'help' then
-    vim.api.nvim_command('wincmd L')
-    vim.api.nvim_buf_set_keymap(0, 'n', 'q', '<CMD>q<CR>', { noremap = true, silent = true })
-end
--- neovim/.config/nvim/lua/numToStr/autocmd.lua
+++ neovim/.config/nvim/lua/numToStr/autocmd.lua
@@ -1,16 +1,45 @@
-local au = require('numToStr.au')
+local A = vim.api
 
-local ft_ev = 'BufNewFile,BufRead'
+local extensions = {
+    ['%.eslintrc$'] = 'json',
+    ['%.eslintignore$'] = 'conf',
+    ['%.prettierrc$'] = 'json',
+    ['%.prettierignore$'] = 'conf',
+    ['%.conf$'] = 'conf',
+    ['.*%.env.*'] = 'sh',
+    ['%.mdx$'] = 'markdown',
+    ['%.mjml$'] = 'html',
+    ['.*tmux.*conf'] = 'tmux',
+}
 
-au.group('MyFileTypes', {
-    { ft_ev, '.eslintrc,.prettierrc,*.json*', 'set ft=json' },
-    { ft_ev, '.eslintignore,.prettierignore,*.conf', 'set ft=conf' },
-    { ft_ev, '*.env*', 'set ft=sh' },
-    { ft_ev, '*.mdx', 'set ft=markdown' },
-    { ft_ev, '*.mjml', 'set ft=html' },
-    { ft_ev, '*tmux*conf', 'set ft=tmux' },
-})
+require('numToStr.au').group('NUMTOSTR', function(aucmd)
+    -- Custom filetypes
+    aucmd('BufNewFile,BufRead', function()
+        local bufnr = A.nvim_get_current_buf()
+        local path = A.nvim_buf_get_name(bufnr)
+        if #path == 0 then
+            return
+        end
+        for ext, ft in pairs(extensions) do
+            if path:match('.*/(.*)$'):find(ext) then
+                A.nvim_buf_set_option(bufnr, 'filetype', ft)
+            end
+        end
+    end)
 
-au.TextYankPost = function()
-    vim.highlight.on_yank({ higroup = 'Visual', timeout = 120 })
-end
+    -- Open help vertically and press q to exit
+    aucmd.BufEnter = {
+        '*.txt',
+        function()
+            if vim.bo.buftype == 'help' then
+                A.nvim_command('wincmd L')
+                A.nvim_buf_set_keymap(0, 'n', 'q', ':q<CR>', { noremap = true, silent = true })
+            end
+        end,
+    }
+
+    -- Highlight the region on yank
+    aucmd.TextYankPost = function()
+        vim.highlight.on_yank({ higroup = 'Visual', timeout = 120 })
+    end
+end)
