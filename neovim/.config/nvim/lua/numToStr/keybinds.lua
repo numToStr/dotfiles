b649076e3a3a8a8a33c0ce5516c3d675242ba470
numToStr <vikasraj1911@gmail.com>
2021-12-09 19:04:32 +0530

    revert default keybinds to use :

--- neovim/.config/nvim/lua/numToStr/keybinds.lua
+++ neovim/.config/nvim/lua/numToStr/keybinds.lua
@@ -17,39 +17,39 @@ K.i('<C-E>', '<ESC>A')
 K.i('<C-A>', '<ESC>I')
 
 -- Fast execute macro
-K.x('<S-q>', '<CMD>normal @@<CR>')
-K.n('<S-q>', '<CMD>normal @@<CR>')
+K.x('<S-q>', ':normal @@<CR>')
+K.n('<S-q>', ':normal @@<CR>')
 
 -- Quickly save the current buffer or all buffers
-K.n('<leader>w', '<CMD>update<CR>')
-K.n('<leader>W', '<CMD>wall<CR>')
+K.n('<leader>w', ':update<CR>')
+K.n('<leader>W', ':wall<CR>')
 
 -- Quit neovim
-K.n('<C-Q>', '<CMD>q<CR>')
+K.n('<C-Q>', ':q<CR>')
 
 -- leader-o/O inserts blank line below/above
 K.n('<leader>o', 'o<ESC>')
 K.n('<leader>O', 'O<ESC>')
 
 -- Move to the next/previous buffer
-K.n('<leader>[', '<CMD>bp<CR>')
-K.n('<leader>]', '<CMD>bn<CR>')
+K.n('<leader>[', ':bp<CR>')
+K.n('<leader>]', ':bn<CR>')
 
 -- Move to last buffer
-K.n("''", '<CMD>b#<CR>')
+K.n("''", ':b#<CR>')
 
 -- Copying the vscode behaviour of making tab splits
-K.n('<C-\\>', '<CMD>vsplit<CR>')
-K.n('<A-\\>', '<CMD>split<CR>')
+K.n('<C-\\>', ':vsplit<CR>')
+K.n('<A-\\>', ':split<CR>')
 
 -- Move line up and down in NORMAL and VISUAL modes
 -- Reference: https://vim.fandom.com/wiki/Moving_lines_up_or_down
-K.n('<C-j>', '<CMD>move .+1<CR>')
-K.n('<C-k>', '<CMD>move .-2<CR>')
-K.v('<C-j>', "<CMD>move '>+1<CR>gv=gv")
-K.v('<C-k>', "<CMD>move '<-2<CR>gv=gv")
+K.n('<C-j>', ':move .+1<CR>')
+K.n('<C-k>', ':move .-2<CR>')
+K.x('<C-j>', ":move '>+1<CR>gv=gv")
+K.x('<C-k>', ":move '<-2<CR>gv=gv")
 
 -- Use operator pending mode to visually select the whole buffer
 -- e.g. dA = delete buffer ALL, yA = copy whole buffer ALL
-K.o('A', '<CMD>normal! mzggVG<CR>`z')
-K.x('A', '<CMD>normal! ggVG<CR>')
+K.o('A', ':<C-U>normal! mzggVG<CR>`z')
+K.x('A', ':<C-U>normal! ggVG<CR>')
