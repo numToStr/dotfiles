eae0c3930f29db8ade7f78a79ce861045e0e78be
numToStr <vikasraj1911@gmail.com>
2021-12-25 12:33:07 +0530

    nvim: increased completion items in nvim-cmp

--- neovim/.config/nvim/lua/numToStr/plugins/lsp/nvim-cmp.lua
+++ neovim/.config/nvim/lua/numToStr/plugins/lsp/nvim-cmp.lua
@@ -9,8 +9,8 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
     },
     sources = cmp.config.sources({
-        { name = 'nvim_lsp', max_item_count = 5 },
-        { name = 'luasnip', max_item_count = 5 },
+        { name = 'nvim_lsp', max_item_count = 10 },
+        { name = 'luasnip', max_item_count = 10 },
         { name = 'path', max_item_count = 10 },
         { name = 'buffer', max_item_count = 10 },
     }),
