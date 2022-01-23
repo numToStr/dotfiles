fa30cd4a1b71c3e84816037f7e4d80a320bf19a6
numToStr <vikasraj1911@gmail.com>
2022-01-23 14:04:58 +0530

    nvim: add gitsigns keybindings
    Ref: https://github.com/lewis6991/gitsigns.nvim/pull/449

--- neovim/.config/nvim/lua/numToStr/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/numToStr/plugins/gitsigns.lua
@@ -4,4 +4,19 @@ require('gitsigns').setup({
         change = { text = '~' },
         changedelete = { text = '=' },
     },
+    on_attach = function(buf)
+        local map = vim.keymap.set
+
+        -- Navigation
+        map('n', ']c', "&diff ? ']c' : '<CMD>Gitsigns next_hunk<CR>'", { buffer = buf, expr = true })
+        map('n', '[c', "&diff ? '[c' : '<CMD>Gitsigns prev_hunk<CR>'", { buffer = buf, expr = true })
+
+        -- Actions
+        map({ 'n', 'v' }, '<leader>hr', '<CMD>Gitsigns reset_hunk<CR>', { buffer = buf })
+        map('n', '<leader>hp', '<CMD>Gitsigns preview_hunk<CR>', { buffer = buf })
+        map('n', '<leader>hS', '<CMD>Gitsigns stage_buffer<CR>', { buffer = buf })
+
+        -- Text object
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { buffer = buf })
+    end,
 })
