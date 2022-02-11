5e9b15f6d1a8d1980ad59a42909fff2635f900ea
numToStr <vikasraj1911@gmail.com>
2022-02-11 11:40:26 +0530

    nvim: ignore proc-macro in rust-analyzer

--- neovim/.config/nvim/lua/numToStr/plugins/lsp/servers.lua
+++ neovim/.config/nvim/lua/numToStr/plugins/lsp/servers.lua
@@ -69,6 +69,13 @@ lsp.rust_analyzer.setup({
                 allFeatures = true,
                 command = 'clippy',
             },
+            procMacro = {
+                ignored = {
+                    ['async-trait'] = { 'async_trait' },
+                    ['napi-derive'] = { 'napi' },
+                    ['async-recursion'] = { 'async_recursion' },
+                },
+            },
         },
     },
 })
