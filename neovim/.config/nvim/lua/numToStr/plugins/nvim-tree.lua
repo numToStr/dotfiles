00c1baa7fcbfcc356b66206018d0d0000c124fa2
numToStr <vikasraj1911@gmail.com>
2022-04-14 15:06:50 +0530

    nvim: update nvim-tree config

--- neovim/.config/nvim/lua/numToStr/plugins/nvim-tree.lua
+++ neovim/.config/nvim/lua/numToStr/plugins/nvim-tree.lua
@@ -1,6 +1,5 @@
 local g = vim.g
 
-g.nvim_tree_indent_markers = 1
 g.nvim_tree_show_icons = {
     git = 1,
     folders = 1,
@@ -41,6 +40,11 @@ require('nvim-tree').setup({
             window_picker = {
                 enable = false,
             },
+        },
+    },
+    renderer = {
+        indent_markers = {
+            enable = true,
         },
     },
 })
