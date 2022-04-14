6117a30dc6249e1ac3dc0990376a64d2748cc882
numToStr <vikasraj1911@gmail.com>
2022-04-14 15:07:23 +0530

    nvim: fix nvim-cmp mappings

--- neovim/.config/nvim/lua/numToStr/plugins/lsp/nvim-cmp.lua
+++ neovim/.config/nvim/lua/numToStr/plugins/lsp/nvim-cmp.lua
@@ -1,13 +1,13 @@
 local cmp = require('cmp')
 
 cmp.setup({
-    mapping = {
+    mapping = cmp.mapping.preset.insert({
         ['<C-e>'] = cmp.config.disable,
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
         ['<C-Space>'] = cmp.mapping.complete(),
-    },
+    }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp', max_item_count = 10 },
         { name = 'luasnip', max_item_count = 10 },
