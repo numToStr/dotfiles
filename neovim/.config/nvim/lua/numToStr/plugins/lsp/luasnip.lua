7b84b0159a8dea01babe8ae6ba8cf5b880d94b9a
numToStr <vikasraj1911@gmail.com>
2022-06-07 14:21:24 +0530

    nvim: entend jsx and tsx to use js snippets

--- neovim/.config/nvim/lua/numToStr/plugins/lsp/luasnip.lua
+++ neovim/.config/nvim/lua/numToStr/plugins/lsp/luasnip.lua
@@ -18,8 +18,8 @@ require('luasnip').config.setup({
 -- Loading any vscode snippets from plugins
 require('luasnip.loaders.from_vscode').lazy_load()
 
--- Mappins to move around inside snippets
-local K = require('numToStr.keymap')
+-- Allow jsx and tsx to use js snippets
+require('luasnip').filetype_extend('javascript', { 'javascriptreact', 'typescriptreact' })
 
 -- Mappins to move around inside snippets
 vim.keymap.set('i', '<C-j>', '<CMD>lua require("luasnip").jump(1)<CR>')
