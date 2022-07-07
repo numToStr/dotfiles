0e7ca2b5f07cf4388ea197b261b4ac10f4921af1
numToStr <vikasraj1911@gmail.com>
2022-07-07 16:30:02 +0530

    nvim: use table as cmd for gitui terminal
    This will load gitui directly instead going through shell.
    See `:h jobstart`

--- neovim/.config/nvim/lua/numToStr/plugins/fterm.lua
+++ neovim/.config/nvim/lua/numToStr/plugins/fterm.lua
@@ -1,3 +1,3 @@
 vim.keymap.set('n', '<A-i>', "<CMD>lua require('FTerm').toggle()<CR>")
 vim.keymap.set('t', '<A-i>', "<C-\\><C-n><CMD>lua require('FTerm').toggle()<CR>")
-vim.keymap.set('n', '<A-g>', '<CMD>lua require("FTerm"):new({ cmd = "gitui" }):open()<CR>')
+vim.keymap.set('n', '<A-g>', '<CMD>lua require("FTerm"):new({ cmd = { "gitui" } }):open()<CR>')
