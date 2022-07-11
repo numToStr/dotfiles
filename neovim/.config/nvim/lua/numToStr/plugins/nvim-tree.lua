c1ad472066a29a18e1436dd7f67684b06c87a4da
numToStr <vikasraj1911@gmail.com>
2022-07-11 22:54:21 +0530

    nvim: disable folder_arrow icon in nvim-tree

--- neovim/.config/nvim/lua/numToStr/plugins/nvim-tree.lua
+++ neovim/.config/nvim/lua/numToStr/plugins/nvim-tree.lua
@@ -28,6 +28,7 @@ require('nvim-tree').setup({
                 git = true,
                 folder = true,
                 file = true,
+                folder_arrow = false,
             },
             glyphs = {
                 default = '',
