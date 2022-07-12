781c9a0a300c8920f2cd1564990509285ead1461
numToStr <vikasraj1911@gmail.com>
2022-07-12 11:13:58 +0530

    nvim: add eslint_d and shellcheck codeactions

--- neovim/.config/nvim/lua/numToStr/plugins/lsp/null-ls.lua
+++ neovim/.config/nvim/lua/numToStr/plugins/lsp/null-ls.lua
@@ -3,11 +3,14 @@ local U = require('numToStr.plugins.lsp.utils')
 
 local fmt = nls.builtins.formatting
 local dgn = nls.builtins.diagnostics
+local cda = nls.builtins.code_actions
 
 -- Configuring null-ls
 nls.setup({
     sources = {
-        -- # FORMATTING #
+        ----------------
+        -- FORMATTING --
+        ----------------
         fmt.trim_whitespace.with({
             filetypes = { 'text', 'zsh', 'toml', 'make', 'conf', 'tmux' },
         }),
@@ -31,12 +34,19 @@ nls.setup({
         fmt.shfmt.with({
             extra_args = { '-i', 4, '-ci', '-sr' },
         }),
-        -- # DIAGNOSTICS #
+        -----------------
+        -- DIAGNOSTICS --
+        -----------------
         dgn.eslint_d,
         dgn.shellcheck,
         dgn.luacheck.with({
             extra_args = { '--globals', 'vim', '--std', 'luajit' },
         }),
+        ------------------
+        -- CODE ACTIONS --
+        ------------------
+        cda.eslint_d,
+        cda.shellcheck,
     },
     on_attach = function(client, bufnr)
         U.fmt_on_save(client, bufnr)
