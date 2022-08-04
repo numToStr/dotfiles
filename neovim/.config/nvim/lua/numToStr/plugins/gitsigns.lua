543707400e09fc1e57d95f6bb08de5aa84bc0cb2
numToStr <vikasraj1911@gmail.com>
2022-08-04 16:30:33 +0530

    nvim: fixed gitsigns bindings

--- neovim/.config/nvim/lua/numToStr/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/numToStr/plugins/gitsigns.lua
@@ -7,9 +7,11 @@ require('gitsigns').setup({
         changedelete = { text = '=' },
     },
     on_attach = function(buf)
+        local opts = { buffer = buf, expr = true, replace_keycodes = false }
+
         -- Navigation
-        map('n', ']c', "&diff ? ']c' : '<CMD>Gitsigns next_hunk<CR>'", { buffer = buf, expr = true })
-        map('n', '[c', "&diff ? '[c' : '<CMD>Gitsigns prev_hunk<CR>'", { buffer = buf, expr = true })
+        map('n', ']c', "&diff ? ']c' : '<CMD>Gitsigns next_hunk<CR>'", opts)
+        map('n', '[c', "&diff ? '[c' : '<CMD>Gitsigns prev_hunk<CR>'", opts)
 
         -- Actions
         map({ 'n', 'v' }, '<leader>hr', '<CMD>Gitsigns reset_hunk<CR>', { buffer = buf })
