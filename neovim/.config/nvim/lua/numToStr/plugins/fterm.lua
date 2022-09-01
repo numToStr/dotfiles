c05b8170e8d8ccca22aadf3b1fc8ab8a6525ea6c
numToStr <vikasraj1911@gmail.com>
2022-09-01 17:21:45 +0530

    nvim: bigger gitui terminal

--- neovim/.config/nvim/lua/numToStr/plugins/fterm.lua
+++ neovim/.config/nvim/lua/numToStr/plugins/fterm.lua
@@ -1,3 +1,10 @@
 vim.keymap.set('n', '<A-i>', "<CMD>lua require('FTerm').toggle()<CR>")
 vim.keymap.set('t', '<A-i>', "<C-\\><C-n><CMD>lua require('FTerm').toggle()<CR>")
-vim.keymap.set('n', '<A-g>', '<CMD>lua require("FTerm"):new({ cmd = { "gitui" } }):open()<CR>')
+vim.keymap.set('n', '<A-g>', function()
+    require('FTerm')
+        :new({
+            cmd = { 'gitui' },
+            dimensions = { height = 0.9, width = 0.9 },
+        })
+        :open()
+end)
